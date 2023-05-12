df3026861286cf76e552953e8c777bb35692143f
Rodrigo Bonacin <rodrigo.bonacin@gmail.com>
2023-05-11 22:22:41 -0400

    .

---
+++ inf335prj51/src/main/java/br/com/inf335prj51/App.java
@@ -0,0 +1,13 @@
+package br.com.inf335prj51;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ inf335prj51/src/test/java/br/com/inf335prj51/AppTest.java
@@ -0,0 +1,20 @@
+package br.com.inf335prj51;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
